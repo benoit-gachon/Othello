65d5201bff5b0ba58ade5520cd8dabf3f575d14b
Benoît <ben.gachon31@gmail.com>
2023-10-20 10:43:35 +0200

    Création et affichage du board

--- board.py
+++ board.py
@@ -2,71 +2,40 @@
 ###       Board        ###
 ##########################
 
-# class Board:
-#     def __init__(self, affichage):
-#         self.affichage = affichage
+class Board:
+    def __init__(self): 
+        self.board = []
+        for _ in range(9):
+            row = [' '] * 9
+            self.board.append(row)
+    
+        self.board[0][1] = "1"
+        self.board[0][2] = "2"
+        self.board[0][3] = "3"
+        self.board[0][4] = "4"
+        self.board[0][5] = "5"
+        self.board[0][6] = "6"
+        self.board[0][7] = "7"
+        self.board[0][8] = "8"
 
-#     def affichage(self):
-#         for row in range(1, 18):
-#             for i in range(1, 33) : 
-#                 if row % 2 ==0:
-#                     if i == 1:
-#                         print("|", end="")
-#                     if i % 4 == 0 :
-#                         print("|", end="")
-#                     else:
-#                         print(" ", end="")
-#                 if row % 2 !=0:
-#                     if i == 1:
-#                         print("+", end="")
-#                     if i % 4 == 0:
-#                         print("+", end="")
-#                     else:
-#                         print("-", end="")
-#             print(" ")
-
-###################################### test 2
-# for row in range(1, 19):
-#     if row == 1:
-#         print("   A   B   C   D   E   F   G   H")
-#     for i in range(1, 34) : 
-#         if i == 1:
-#             print(1,2,3,4,5,6,7,8, sep="\n")
-#         if row % 2 + 1 ==1:
-#             if i == 2:
-#                 print("|", end="")
-#             if i % 4 +1 == 1 :
-#                 print("|", end="")
-#             else:
-#                 print(" ", end="")
-#         if row % 2 ==0:
-#             if i == 2:
-#                 print("+", end="")
-#             if i % 4 +1 == 1:
-#                 print("+", end="")
-#             else:
-#                 print("-", end="")
-#     print(" ")
+        self.board[1][0] = "1"
+        self.board[2][0] = "2"
+        self.board[3][0] = "3"
+        self.board[4][0] = "4"
+        self.board[5][0] = "5"
+        self.board[6][0] = "6"
+        self.board[7][0] = "7"
+        self.board[8][0] = "8"
 
 
+        self.board[4][4] = 'X'
+        self.board[4][5] = 'O'
+        self.board[5][4] = 'O'
+        self.board[5][5] = 'X'
+    
+    def display_board(self):
+        for row in self.board:
+            print(" | ".join(row))
+            print("-" * 35)
+        return self.board
 
-def init_board():
-    board = [[' ' for _ in range(8)] for _ in range(8)]
-    board[3][3] = 'X'
-    board[3][4] = 'O'
-    board[4][3] = 'O'
-    board[4][4] = 'X'
-    return board
-
-
-
-def display_board(board):
-    for element in board[0][0]:
-        print("A   B   C   D   E   F   G   H")
-    for element in board[0][1]:
-    for row in board:
-        print(" | ".join(row))
-        print("-" * 30)
-
-
-print(display_board(init_board()))
